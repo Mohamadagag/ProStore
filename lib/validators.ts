f351412919dbfe051e430c3cab660315574d9a74
Mohamad Agag <mohamad.agag007@gmail.com>
2025-06-18 02:42:44 +0300

    Added admin products page and image upload

--- lib/validators.ts
+++ lib/validators.ts
@@ -5,7 +5,7 @@ import { PAYMENT_METHODS } from './constants'
 const currency = z.string().refine((value) => /^\d+(\.\d{2})?$/.test(formatNumberWithDecimal(Number(value))), 'Price must have exactly two decimal places')
 
 // Schema for inserting products
-export const InsertProductSchema = z.object({
+export const insertProductSchema = z.object({
     name: z.string().min(3, 'Name must be at least 3 characters'),
     slug: z.string().min(3, 'Slug must be at least 3 characters'),
     category: z.string().min(3, 'Category must be at least 3 characters'),
@@ -16,6 +16,11 @@ export const InsertProductSchema = z.object({
     isFeatured: z.boolean(),
     banner: z.string().nullable(),
     price: currency,
+})
+
+// Schema for updating products
+export const updateProductSchema = insertProductSchema.extend({
+    id: z.string().min(1, 'ID is required'),
 })
 
 
